e736bc639832d4990609c8e7ccd29f78cb3ddfe8
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-02-03 23:53:18 +0100

    Fix duplicate mod error on servers #6

--- src/main/java/com/supermartijn642/scarecrowsterritory/core/CoreMod.java
+++ src/main/java/com/supermartijn642/scarecrowsterritory/core/CoreMod.java
@@ -1,38 +1,21 @@
 package com.supermartijn642.scarecrowsterritory.core;
 
-import net.minecraftforge.fml.relauncher.CoreModManager;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
-import org.apache.logging.log4j.LogManager;
 import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.Mixins;
 
-import java.io.File;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.security.CodeSource;
 import java.util.Map;
 
 /**
  * Created 1/16/2021 by SuperMartijn642
  */
 @IFMLLoadingPlugin.MCVersion("1.12.2")
+@IFMLLoadingPlugin.Name("Scarecrow's Territory Plugin")
 public class CoreMod implements IFMLLoadingPlugin {
 
     public CoreMod() {
         MixinBootstrap.init();
-
-        CodeSource codeSource = this.getClass().getProtectionDomain().getCodeSource();
-        if (codeSource != null) {
-            URL location = codeSource.getLocation();
-            try {
-                File file = new File(location.toURI());
-                if (file.isFile()) {
-                    CoreModManager.getReparseableCoremods().remove(file.getName());
-                }
-            } catch (URISyntaxException ignored) {}
-        } else {
-            LogManager.getLogger().warn("No CodeSource, if this is not a development environment we might run into problems!");
-            LogManager.getLogger().warn(this.getClass().getProtectionDomain());
-        }
+        Mixins.addConfiguration("mixins.scarecrowsterritory.json");
     }
 
     @Override

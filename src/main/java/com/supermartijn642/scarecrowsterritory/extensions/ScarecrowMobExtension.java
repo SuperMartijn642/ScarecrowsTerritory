20d7914807dcd81acc58b7642a0fc0bf27bae65e
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-08-03 15:16:03 +0200

    Properly allow mobs spawned by a scarecrow to despawn when there's no player in the same dimension

---
+++ src/main/java/com/supermartijn642/scarecrowsterritory/extensions/ScarecrowMobExtension.java
@@ -0,0 +1,11 @@
+package com.supermartijn642.scarecrowsterritory.extensions;
+
+/**
+ * Created 03/08/2023 by SuperMartijn642
+ */
+public interface ScarecrowMobExtension {
+
+    void scarecrowsterritory$setSpawnedByScarecrow();
+
+    boolean scarecrowsterritory$wasSpawnedByScarecrow();
+}

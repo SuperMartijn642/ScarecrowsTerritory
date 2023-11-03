a5f8135eead8b52cb6017f2d60d8e98f6bf5b772
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-11-03 23:32:43 +0100

    Rework CommonRuleEvaluatorMixin

--- src/main/java/com/supermartijn642/scarecrowsterritory/mixin/in_control/CommonRuleEvaluatorMixin.java
+++ src/main/java/com/supermartijn642/scarecrowsterritory/mixin/in_control/CommonRuleEvaluatorMixin.java
@@ -8,8 +8,9 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.LevelAccessor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import java.util.function.BiFunction;
 
 /**
  * Created 03/11/2023 by SuperMartijn642
@@ -17,16 +18,24 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(value = CommonRuleEvaluator.class, remap = false)
 public class CommonRuleEvaluatorMixin {
 
-    @Inject(
-        method = "lambda$addGameStageCheck$106(Ljava/lang/String;Ljava/lang/Object;Lmcjty/incontrol/tools/rules/IEventQuery;)Ljava/lang/Boolean",
-        at = @At("HEAD"),
-        cancellable = true,
+    @ModifyArg(
+        method = "addGameStageCheck",
+        at = @At(
+            value = "INVOKE",
+            target = "Ljava/util/List;add(Ljava/lang/Object;)Z"
+        ),
+        index = 0,
         remap = false
     )
-    private void scarecrowGameStageCheck(String stage, Object event, IEventQuery<Object> query, CallbackInfoReturnable<Boolean> ci){
-        LevelAccessor level = query.getWorld(event);
-        BlockPos pos = query.getPos(event);
-        if(level != null && pos != null && ScarecrowTracker.isScarecrowInRange(level, pos.getCenter(), ScarecrowsTerritoryConfig.passiveMobRange.get()))
-            ci.setReturnValue(true);
+    private Object scarecrowGameStageCheck(Object o){
+        //noinspection unchecked,rawtypes
+        BiFunction<Object,IEventQuery,Boolean> check = (BiFunction<Object,IEventQuery,Boolean>)o;
+        return (BiFunction<Object,IEventQuery,Boolean>)(event, query) -> {
+            LevelAccessor level = query.getWorld(event);
+            BlockPos pos = query.getPos(event);
+            if(level != null && pos != null && ScarecrowTracker.isScarecrowInRange(level, pos.getCenter(), ScarecrowsTerritoryConfig.passiveMobRange.get()))
+                return true;
+            return check.apply(event, query);
+        };
     }
 }

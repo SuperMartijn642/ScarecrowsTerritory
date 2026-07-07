cb8880063e4da69021880c9735227a18c1cdcd53
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-07 21:31:44 +0200

    Update to Minecraft 26.2

--- src/main/java/com/supermartijn642/scarecrowsterritory/MobSpawningUtil.java
+++ src/main/java/com/supermartijn642/scarecrowsterritory/MobSpawningUtil.java
@@ -15,6 +15,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.world.phys.Vec3;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -96,7 +97,7 @@ public class MobSpawningUtil {
                     double spawnXCenter = (double)spawnX + 0.5D;
                     double spawnZCenter = (double)spawnZ + 0.5D;
 
-                    if(!ScarecrowTracker.isScarecrowInRange(level, pos.getCenter(), ScarecrowsTerritoryConfig.passiveMobRange.get()))
+                    if(!ScarecrowTracker.isScarecrowInRange(level, Vec3.atCenterOf(pos), ScarecrowsTerritoryConfig.passiveMobRange.get()))
                         continue;
 
                     if(spawner == null){

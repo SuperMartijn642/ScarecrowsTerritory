e7b8a2956192571c3da02e577a2c2b1e7fe39a01
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-09-18 13:34:43 +0200

    Separate `ScarecrowTracker` client and server side #49

--- src/main/java/com/supermartijn642/scarecrowsterritory/ScarecrowsTerritoryClient.java
+++ src/main/java/com/supermartijn642/scarecrowsterritory/ScarecrowsTerritoryClient.java
@@ -2,6 +2,7 @@ package com.supermartijn642.scarecrowsterritory;
 
 import com.supermartijn642.core.registry.ClientRegistrationHandler;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientChunkEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.world.item.DyeColor;
 
@@ -13,6 +14,8 @@ public class ScarecrowsTerritoryClient implements ClientModInitializer {
     @Override
     public void onInitializeClient(){
         ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> ScarecrowTracker.get(true).onClientLevelChange(null));
+        ClientChunkEvents.CHUNK_LOAD.register(ScarecrowTracker.get(true)::onChunkLoad);
+        ClientChunkEvents.CHUNK_UNLOAD.register(ScarecrowTracker.get(true)::onChunkUnload);
 
         ClientRegistrationHandler handler = ClientRegistrationHandler.get("scarecrowsterritory");
         for(ScarecrowType type : ScarecrowType.values()){

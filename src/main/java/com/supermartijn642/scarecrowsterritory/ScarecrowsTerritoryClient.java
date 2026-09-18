d38861f3256b49da23c0f83a9acb9a07801dbab2
Ivan <60302361+depocoder@users.noreply.github.com>
2026-09-18 10:44:01 +0200

    Fix client levels being kept alive by `ScarecrowTracker` (#49)

--- src/main/java/com/supermartijn642/scarecrowsterritory/ScarecrowsTerritoryClient.java
+++ src/main/java/com/supermartijn642/scarecrowsterritory/ScarecrowsTerritoryClient.java
@@ -2,6 +2,8 @@ package com.supermartijn642.scarecrowsterritory;
 
 import com.supermartijn642.core.registry.ClientRegistrationHandler;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientWorldEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.world.item.DyeColor;
 
 /**
@@ -11,6 +13,9 @@ public class ScarecrowsTerritoryClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient(){
+        ClientWorldEvents.AFTER_CLIENT_WORLD_CHANGE.register((client, level) -> ScarecrowTracker.onClientLevelChange(level));
+        ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> ScarecrowTracker.onClientLevelChange(null));
+
         ClientRegistrationHandler handler = ClientRegistrationHandler.get("scarecrowsterritory");
         for(ScarecrowType type : ScarecrowType.values()){
             for(DyeColor color : DyeColor.values()){

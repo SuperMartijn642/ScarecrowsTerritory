de0dd947050defd82975481a83cb3513022fa7fe
Ivan <60302361+depocoder@users.noreply.github.com>
2026-09-18 11:44:01 +0300

    Fix client levels being kept alive by `ScarecrowTracker` (#49)

--- src/main/java/com/supermartijn642/scarecrowsterritory/ScarecrowsTerritoryClient.java
+++ src/main/java/com/supermartijn642/scarecrowsterritory/ScarecrowsTerritoryClient.java
@@ -1,6 +1,8 @@
 package com.supermartijn642.scarecrowsterritory;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLevelEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 
 /**
  * Created 7/11/2020 by SuperMartijn642
@@ -9,5 +11,8 @@ public class ScarecrowsTerritoryClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient(){
+        // Fabric doesn't fire chunk unload events when the client switches or leaves a level, so drop stale client levels here
+        ClientLevelEvents.AFTER_CLIENT_LEVEL_CHANGE.register((client, level) -> ScarecrowTracker.onClientLevelChange(level));
+        ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> ScarecrowTracker.onClientLevelChange(null));
     }
 }
